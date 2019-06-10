5c1580e3433a5809aec7cca1af8615eeb1d62045
qianbin <cola.tin.com@gmail.com>
2019-06-10 15:29:43 +0800

    add class style Connex constructor

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import { newVendor } from './vendor'
 import { Driver } from './driver'
 import { version as connexVersion } from '@vechain/connex/package.json'
 
-export function newConnex(
-    driver: Driver,
-    genesis: Connex.Thor.Block,
-    initialHead?: Connex.Thor.Status['head']
-): Connex {
-    return {
-        thor: newThor(driver, genesis, initialHead),
-        vendor: newVendor(driver),
-        version: connexVersion
+export class ConnexImpl implements Connex {
+    public readonly version = connexVersion
+    public readonly thor: Connex.Thor
+    public readonly vendor: Connex.Vendor
+
+    constructor(
+        driver: Driver,
+        genesis: Connex.Thor.Block,
+        initialHead?: Connex.Thor.Status['head']
+    ) {
+        this.thor = newThor(driver, genesis, initialHead)
+        this.vendor = newVendor(driver)
     }
 }
 
